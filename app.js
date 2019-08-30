104539f407ac6a16b82d35d82841715258f15780
Juliana Augusta <juliana.a.da.silva@avanade.com>
2019-08-30 15:10:34 -0300

    Novo layout de tela

--- app.js
+++ app.js
@@ -1,78 +1,91 @@
-do {
- var options=0;
-  var menu = '1. Criptografar\n2. Descriptografar\n3. SAIR\n';
-  options = parseInt(prompt(menu));
-
-  if (options === 1) {
-    var textToEncrip = prompt('Digite a frase para criptografar');
-
-    function cipher(){
-
-      var result = "";
+// var textToEncrip = document.getElementById('input-text-cipher-decipher').value;
+// console.log(textToEncrip);
 
 
-      for (var i = 0; i < textToEncrip.length; i++){
-        var word = textToEncrip[i];
 
-      if ((word.charCodeAt(0) >= 65) && (word.charCodeAt(0) <= 90))
-      {
-        var num = word.charCodeAt(0) - 65;
-        result += String.fromCharCode(((num + 7) % 26) + 65);
-      }
-
-      else if ((word.charCodeAt(0) >= 97) && (word.charCodeAt(0) <= 122))
-      {
-        var num = word.charCodeAt(0) - 97;
-        result += String.fromCharCode(((num + 7) % 26) + 97);
-      }
-
-      else if (word.charCodeAt(0) === 32)
-      {
-        var num = word.charCodeAt(0);
-        result += String.fromCharCode(num);
-      }
-    }
-  return result;
-}
-console.log (cipher());
-cipher ();
-document.getElementById('crip').innerHTML = ("Sua frase decifrada: "+ cipher ());
-// alert ("Sua frase decifrada: "+ cipher ());
-
+function cipherText(textToEncrip) {
+    console.log(textToEncrip);
 }
 
-if (options === 2) {
-
-  var textToDecrypt = prompt('Digite a frase para criptografar');
-
-    function decipher(){
-
-      var result = "";
-
-      for (var i = 0; i < textToDecrypt.length; i++){
-        var word = textToDecrypt[i];
-      if ((word.charCodeAt(0) >= 65) && (word.charCodeAt(0) <= 90))
-      {
-        var num = word.charCodeAt(0) - 65;
-        result += String.fromCharCode(((num + 19) % 26) + 65);
-      }
-      else if ((word.charCodeAt(0) >= 97) && (word.charCodeAt(0) <= 122))
-      {
-        var num = word.charCodeAt(0) - 97;
-        result += String.fromCharCode(((num + 19) % 26) + 97);
-      }
-      else if (word.charCodeAt(0) === 32)
-      {
-        var num = word.charCodeAt(0);
-        result += String.fromCharCode(num);
-      }
-      }
-  return result;
-}
-console.log (decipher());
-decipher();
-// alert ("Sua frase decifrada: "+ decipher());
-document.getElementById('descrip').innerHTML = ("Sua frase decifrada: "+ decipher());
+function decipherText() {
+    console.log("decifrar")
 }
 
-}while (options !== 3);
+// do {
+//  var options=0;
+//   var menu = '1. Criptografar\n2. Descriptografar\n3. SAIR\n';
+//   options = parseInt(prompt(menu));
+
+//   if (options === 1) {
+//     var textToEncrip = prompt('Digite a frase para criptografar');
+
+//     function cipher(){
+
+//       var result = "";
+
+
+//       for (var i = 0; i < textToEncrip.length; i++){
+//         var word = textToEncrip[i];
+
+//       if ((word.charCodeAt(0) >= 65) && (word.charCodeAt(0) <= 90))
+//       {
+//         var num = word.charCodeAt(0) - 65;
+//         result += String.fromCharCode(((num + 7) % 26) + 65);
+//       }
+
+//       else if ((word.charCodeAt(0) >= 97) && (word.charCodeAt(0) <= 122))
+//       {
+//         var num = word.charCodeAt(0) - 97;
+//         result += String.fromCharCode(((num + 7) % 26) + 97);
+//       }
+
+//       else if (word.charCodeAt(0) === 32)
+//       {
+//         var num = word.charCodeAt(0);
+//         result += String.fromCharCode(num);
+//       }
+//     }
+//   return result;
+// }
+// console.log (cipher());
+// cipher ();
+// document.getElementById('crip').innerHTML = ("Sua frase decifrada: "+ cipher ());
+// // alert ("Sua frase decifrada: "+ cipher ());
+
+// }
+
+// if (options === 2) {
+
+//   var textToDecrypt = prompt('Digite a frase para criptografar');
+
+//     function decipher(){
+
+//       var result = "";
+
+//       for (var i = 0; i < textToDecrypt.length; i++){
+//         var word = textToDecrypt[i];
+//       if ((word.charCodeAt(0) >= 65) && (word.charCodeAt(0) <= 90))
+//       {
+//         var num = word.charCodeAt(0) - 65;
+//         result += String.fromCharCode(((num + 19) % 26) + 65);
+//       }
+//       else if ((word.charCodeAt(0) >= 97) && (word.charCodeAt(0) <= 122))
+//       {
+//         var num = word.charCodeAt(0) - 97;
+//         result += String.fromCharCode(((num + 19) % 26) + 97);
+//       }
+//       else if (word.charCodeAt(0) === 32)
+//       {
+//         var num = word.charCodeAt(0);
+//         result += String.fromCharCode(num);
+//       }
+//       }
+//   return result;
+// }
+// console.log (decipher());
+// decipher();
+// // alert ("Sua frase decifrada: "+ decipher());
+// document.getElementById('descrip').innerHTML = ("Sua frase decifrada: "+ decipher());
+// }
+
+// }while (options !== 3);
