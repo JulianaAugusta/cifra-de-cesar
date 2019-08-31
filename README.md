c4c8a49affd7301c6519a1ab710ba32b4f3a5786
Juliana Augusta <juliana.a.da.silva@avanade.com>
2019-08-31 00:10:05 -0300

    Refactory template, algorítmo e controlador

--- README.md
+++ README.md
@@ -7,15 +7,27 @@ Crie uma página web que peça, por meio de um prompt(), uma frase para o usuár
 
 ----
 ## Algorítmo
-1. Usuário recebe uma mensagem com as opções 1-Criptografar 2-Descriptografar 3-Sair.
-2. Ao escolher a opção para Criptografar o programa entra na função Cipher.
+1. Usuário digita sua mensagem no input de texto.
+2. Ele escolhe entre as duas opções criptografar ou descriptografar.
+3.1 Ao escolher a opção para Criptografar o programa entra na função Cipher.
  * A função verifica onde aquele caractere se encontra dentro da tabela ASCII.
  * Caso o caractere seja minúsculo ele aplica a fórmula: (((num + 7) % 26) + 65).
  * Caso o caractere seja maiúsculo ele aplica a fórmula: (((num + 7) % 26) + 97).
  * Caso o caractere seja um espaço ele permanece como está.
-3. Ao escolher a opção para Descriptografar o programa entra na função Decipher.
+ * É exibido o resultado na caixa de texto.
+3.2 Ao escolher a opção para Descriptografar o programa entra na função Decipher.
  * A função verifica onde aquele caractere se encontra dentro da tabela ASCII.
  * Caso o caractere seja minúsculo ele aplica a fórmula: (((num + 19) % 26) + 65).
  * Caso o caractere seja maiúsculo ele aplica a fórmula: (((num + 19) % 26) + 97).
  * Caso o caractere seja um espaço ele permanece como está.
-4. Ao escolher a opção sair, finaliza o programa.
+ * É exibido o resultado na caixa de texto.
+4. Ao clicar no botão de limpar o texto o input é limpo.
+---
+##Refactory
+1. Agora implementei a página em HTML5 e o usuário irá interagir com ela. Tem uma nova aparência e não uso mais o prompt para realizar ações.
+2. O código foi melhorado e o algoritmo mudado. Há funcionalidade nos botões.
+3. É código vanilla! Todo em JavaScript puro!
+
+---
+##Roadmap
+1. Implementação de uma cifra que aceite acentuação;
--- app.js
+++ app.js
@@ -1,91 +1,48 @@
-// var textToEncrip = document.getElementById('input-text-cipher-decipher').value;
-// console.log(textToEncrip);
+function cipherText() {
+    var textCipher = document.getElementById('input-text-cipher-decipher').value;
+    var result = "";
 
+    for (var i = 0; i < textCipher.length; i++) {
+        var word = textCipher[i];
 
-
-function cipherText(textToEncrip) {
-    console.log(textToEncrip);
+        if ((word.charCodeAt(0) >= 65) && (word.charCodeAt(0) <= 90)) {
+            var num = word.charCodeAt(0) - 65;
+            result += String.fromCharCode(((num + 7) % 26) + 65);
+        } else if ((word.charCodeAt(0) >= 97) && (word.charCodeAt(0) <= 122)) {
+            var num = word.charCodeAt(0) - 97;
+            result += String.fromCharCode(((num + 7) % 26) + 97);
+        } else if (word.charCodeAt(0) === 32) {
+            var num = word.charCodeAt(0);
+            result += String.fromCharCode(num);
+        }
+    }
+    document.getElementById('cipher-result').innerHTML = result;
+    return result;
 }
+
 
 function decipherText() {
-    console.log("decifrar")
+    var textDecipher = document.getElementById('input-text-cipher-decipher').value;
+    var result = "";
+
+    for (var i = 0; i < textDecipher.length; i++) {
+        var word = textDecipher[i];
+        if ((word.charCodeAt(0) >= 65) && (word.charCodeAt(0) <= 90)) {
+            var num = word.charCodeAt(0) - 65;
+            result += String.fromCharCode(((num + 19) % 26) + 65);
+        } else if ((word.charCodeAt(0) >= 97) && (word.charCodeAt(0) <= 122)) {
+            var num = word.charCodeAt(0) - 97;
+            result += String.fromCharCode(((num + 19) % 26) + 97);
+        } else if (word.charCodeAt(0) === 32) {
+            var num = word.charCodeAt(0);
+            result += String.fromCharCode(num);
+        }
+    }
+    document.getElementById('cipher-result').innerHTML = result;
+    return result;
 }
 
-// do {
-//  var options=0;
-//   var menu = '1. Criptografar\n2. Descriptografar\n3. SAIR\n';
-//   options = parseInt(prompt(menu));
-
-//   if (options === 1) {
-//     var textToEncrip = prompt('Digite a frase para criptografar');
-
-//     function cipher(){
-
-//       var result = "";
-
-
-//       for (var i = 0; i < textToEncrip.length; i++){
-//         var word = textToEncrip[i];
-
-//       if ((word.charCodeAt(0) >= 65) && (word.charCodeAt(0) <= 90))
-//       {
-//         var num = word.charCodeAt(0) - 65;
-//         result += String.fromCharCode(((num + 7) % 26) + 65);
-//       }
-
-//       else if ((word.charCodeAt(0) >= 97) && (word.charCodeAt(0) <= 122))
-//       {
-//         var num = word.charCodeAt(0) - 97;
-//         result += String.fromCharCode(((num + 7) % 26) + 97);
-//       }
-
-//       else if (word.charCodeAt(0) === 32)
-//       {
-//         var num = word.charCodeAt(0);
-//         result += String.fromCharCode(num);
-//       }
-//     }
-//   return result;
-// }
-// console.log (cipher());
-// cipher ();
-// document.getElementById('crip').innerHTML = ("Sua frase decifrada: "+ cipher ());
-// // alert ("Sua frase decifrada: "+ cipher ());
-
-// }
-
-// if (options === 2) {
-
-//   var textToDecrypt = prompt('Digite a frase para criptografar');
-
-//     function decipher(){
-
-//       var result = "";
-
-//       for (var i = 0; i < textToDecrypt.length; i++){
-//         var word = textToDecrypt[i];
-//       if ((word.charCodeAt(0) >= 65) && (word.charCodeAt(0) <= 90))
-//       {
-//         var num = word.charCodeAt(0) - 65;
-//         result += String.fromCharCode(((num + 19) % 26) + 65);
-//       }
-//       else if ((word.charCodeAt(0) >= 97) && (word.charCodeAt(0) <= 122))
-//       {
-//         var num = word.charCodeAt(0) - 97;
-//         result += String.fromCharCode(((num + 19) % 26) + 97);
-//       }
-//       else if (word.charCodeAt(0) === 32)
-//       {
-//         var num = word.charCodeAt(0);
-//         result += String.fromCharCode(num);
-//       }
-//       }
-//   return result;
-// }
-// console.log (decipher());
-// decipher();
-// // alert ("Sua frase decifrada: "+ decipher());
-// document.getElementById('descrip').innerHTML = ("Sua frase decifrada: "+ decipher());
-// }
-
-// }while (options !== 3);
+function cleanText() {
+    document.getElementById('input-text-cipher-decipher').value = '';
+    return
+}
