b7fddf4ef1ab4d1a1df394cfd5b236ba9888d364
Juliana Augusta <juliana.a.da.silva@avanade.com>
2019-08-31 00:50:42 -0300

    Refatoração - Um função chama outra que cuida apenas da parte de fazer a criptografia

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 function cipherText() {
-    var textCipher = document.getElementById('input-text-cipher-decipher').value;
+    var textToCipher = document.getElementById('input-text-cipher-decipher').value;
+    encryptData(textToCipher);
+}
+
+function encryptData(letter) {
     var result = "";
 
-    for (var i = 0; i < textCipher.length; i++) {
-        var word = textCipher[i];
+    for (var i = 0; i < letter.length; i++) {
+        var letterToCipher = letter[i];
 
-        if ((word.charCodeAt(0) >= 65) && (word.charCodeAt(0) <= 90)) {
-            var num = word.charCodeAt(0) - 65;
+        if ((letterToCipher.charCodeAt(0) >= 65) && (letterToCipher.charCodeAt(0) <= 90)) {
+            var num = letterToCipher.charCodeAt(0) - 65;
             result += String.fromCharCode(((num + 7) % 26) + 65);
-        } else if ((word.charCodeAt(0) >= 97) && (word.charCodeAt(0) <= 122)) {
-            var num = word.charCodeAt(0) - 97;
+        } else if ((letterToCipher.charCodeAt(0) >= 97) && (letterToCipher.charCodeAt(0) <= 122)) {
+            var num = letterToCipher.charCodeAt(0) - 97;
             result += String.fromCharCode(((num + 7) % 26) + 97);
-        } else if (word.charCodeAt(0) === 32) {
-            var num = word.charCodeAt(0);
+        } else if (letterToCipher.charCodeAt(0) === 32) {
+            var num = letterToCipher.charCodeAt(0);
             result += String.fromCharCode(num);
         }
     }
@@ -23,18 +27,23 @@ function cipherText() {
 
 function decipherText() {
     var textDecipher = document.getElementById('input-text-cipher-decipher').value;
+    decryptData(textDecipher);
+}
+
+function decryptData(letter) {
     var result = "";
 
-    for (var i = 0; i < textDecipher.length; i++) {
-        var word = textDecipher[i];
-        if ((word.charCodeAt(0) >= 65) && (word.charCodeAt(0) <= 90)) {
-            var num = word.charCodeAt(0) - 65;
+    for (var i = 0; i < letter.length; i++) {
+        var letterToDecipher = letter[i];
+
+        if ((letterToDecipher.charCodeAt(0) >= 65) && (letterToDecipher.charCodeAt(0) <= 90)) {
+            var num = letterToDecipher.charCodeAt(0) - 65;
             result += String.fromCharCode(((num + 19) % 26) + 65);
-        } else if ((word.charCodeAt(0) >= 97) && (word.charCodeAt(0) <= 122)) {
-            var num = word.charCodeAt(0) - 97;
+        } else if ((letterToDecipher.charCodeAt(0) >= 97) && (letterToDecipher.charCodeAt(0) <= 122)) {
+            var num = letterToDecipher.charCodeAt(0) - 97;
             result += String.fromCharCode(((num + 19) % 26) + 97);
-        } else if (word.charCodeAt(0) === 32) {
-            var num = word.charCodeAt(0);
+        } else if (letterToDecipher.charCodeAt(0) === 32) {
+            var num = letterToDecipher.charCodeAt(0);
             result += String.fromCharCode(num);
         }
     }
